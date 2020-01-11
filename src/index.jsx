c52c7f38f36d44630cea9918b795d16bd37d51d4
Claire Froelich <claire.froelich@gmail.com>
2020-01-11 11:25:37 -0500

    add redux-promise
    for baked-in promise resolution

--- src/index.jsx
+++ src/index.jsx
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { logger } from 'redux-logger';
+import reduxPromise from 'redux-promise';
 
 // internal modules
 import App from './components/app';
@@ -15,7 +16,7 @@ const reducers = combineReducers({
 });
 
 // apply middlewares
-const middlewares = applyMiddleware(logger);
+const middlewares = applyMiddleware(logger, reduxPromise);
 
 // render an instance of the component in the DOM
 ReactDOM.render(
